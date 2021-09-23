8925ef1a315e50b9bfd300c2b5e3e516bbbfb769
Erik Albuquerquer <eriksilv.77@gmail.com>
2021-09-23 19:33:23 -0300

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,30 @@
+import { useState } from 'react';
+
+import { TransactionsProvider } from './hooks/useTransactions';
+
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+import { GlobalStyles } from './styles/global';
+
 export function App() {
-	return <h1>Hello, word!</h1>;
+	const [ isNewTransactionModalOpen, setIsNewTransactionModalOpen ] = useState(false);
+
+	function handleOpenNewTransactionModal() {
+		setIsNewTransactionModalOpen(true);
+	}
+
+	function handleCloseNewTransactionModal() {
+		setIsNewTransactionModalOpen(false);
+	}
+	return (
+		<TransactionsProvider>
+			<Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+			<Dashboard />
+
+			<NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
+			<GlobalStyles />
+		</TransactionsProvider>
+	);
 }
